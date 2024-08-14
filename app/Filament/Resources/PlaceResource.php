2feeeae653f5a7cab61bc403fc89ffb98bddc929
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-14 13:51:15 +0300

    add

--- app/Filament/Resources/PlaceResource.php
+++ app/Filament/Resources/PlaceResource.php
@@ -117,7 +117,7 @@ class PlaceResource extends Resource
                 TextColumn::make('location')
                 ->label('العنوان')
                 ->searchable()
-                ->link('location')
+                ->link('blank')
                 ->copyable(),
                 TextColumn::make('rating')
                 ->label('التقيم')
