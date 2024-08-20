2da2ad70e92feac6e357d7139a62046372afce9b
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-20 19:57:02 +0300

    wadyy

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -14,28 +14,26 @@ class TeamController extends Controller
         $month = Carbon::now()->month;
         $year = Carbon::now()->year;
         $daysInMonth = Carbon::create($year, $month)->daysInMonth;
-        for ($day = 1; $day <= $daysInMonth; $day++) {
-            $date = Carbon::create($year, $month, $day)->format('Y-m-d');
-            foreach($volunteers as $volunteer){
-                $history = $volunteer->histories()->wherePivot('volunteer_id', $volunteer)->where('date',$date)->get()->first();
-                return response([$history, $date]);
-                foreach($volunteer->events as $event){
-                    $eventDate = $event->date;
-                    if($eventDate == $date){
-                            if($history->date != $eventDate){
-                                $history = new History();
-                                $history->count = 1; 
-                                $history->date = $date; 
-                                $history->save();
-                                $volunteer->histories()->attach($history->id);
+        foreach($volunteers as $volunteer){
+            for ($day = 1; $day <= $daysInMonth; $day++) {
+                $date = Carbon::create($year, $month, $day)->format('Y-m-d');
+                    foreach($volunteer->events as $event){
+                        $eventDate = $event->date;
+                        if($eventDate == $date){ 
+                             foreach($volunteer->histories as $history) {
+                                if($history->date != $eventDate ){
+                                    $history = new History();
+                                    $history->date =  $date;
+                                    $history->count =  1;
+                                    $history->save();
+                                    $volunteer->histories()->attach($history->id);
+                                }
+
+                             }
                         }
                         
                     }
-                    
-                }
             }
-
-
         }
         
         return view('vol.masaol',[
