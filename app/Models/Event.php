cfcf13fc027f92ecbd423b4457d93a2a05f75bc4
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-21 14:14:34 +0300

    wadyy

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use App\Models\Volunteer;
+use App\Models\Contribution;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
@@ -43,7 +45,43 @@ class Event extends Model implements HasMedia
         return LogOptions::defaults();
     }
 
+// Register model event listeners
+protected static function boot()
+{
+    parent::boot();
 
+    static::created(function ($event) {
+        // Get all volunteers
+        $volunteers = Volunteer::all();
+    
+        foreach ($volunteers as $volunteer) {
+            if ($volunteer->events->isNotEmpty()) {
+                foreach ($volunteer->events as $event) {
+                    $day = Carbon::create($event->date)->format('d');
+                    $month = Carbon::create($event->date)->format('m');
+                    $year = Carbon::create($event->date)->format('Y');
+
+                    $contribution = Contribution::where('volunteer_id', $volunteer->id)
+                        ->where('year', $year)
+                        ->where('month', $month)
+                        ->first();
+
+                    if ($contribution) {
+                        $contribution->$day = 1; // Update the specific day
+                        $contribution->save();
+                    } else {
+                        $contribution = new Contribution;
+                        $contribution->volunteer_id = $volunteer->id;
+                        $contribution->year = $year;
+                        $contribution->month = $month;
+                        $contribution->$day = 1; // Set the specific day
+                        $contribution->save();
+                    }
+                }
+            }
+        }
+    });
+}
 
 
 }
