0d2e5f4d0c7cf5894ab4cc4af9da86f59392e2ba
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-21 14:18:36 +0300

    wadyy

--- app/Filament/Resources/EventResource/Pages/CreateEvent.php
+++ app/Filament/Resources/EventResource/Pages/CreateEvent.php
@@ -2,12 +2,21 @@
 
 namespace App\Filament\Resources\EventResource\Pages;
 
-use App\Filament\Resources\EventResource;
 use Filament\Actions;
+use Filament\Actions\CreateAction;
+use App\Filament\Resources\EventResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEvent extends CreateRecord
 {
     protected static string $resource = EventResource::class;
+    protected function getActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->successRedirectUrl(fn ($record) => route('team.masaol'))
+                ->label('Create Event'),
+        ];
+    }
     
 }
