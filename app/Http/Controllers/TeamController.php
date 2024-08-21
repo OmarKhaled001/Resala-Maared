4519bc429a651ac33f5b8e36ea0f4a3e05a417a1
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-21 11:44:07 +0300

    wadyy

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -14,27 +14,7 @@ class TeamController extends Controller
         $month = Carbon::now()->month;
         $year = Carbon::now()->year;
         $daysInMonth = Carbon::create($year, $month)->daysInMonth;
-        for ($day = 1; $day <= $daysInMonth; $day++) {
-            foreach($volunteers as $volunteer){
-                $date = Carbon::create($year, $month, $day)->format('Y-m-d');
-                    foreach($volunteer->events as $event){
-                        $eventDate = $event->date;
-                        if($eventDate == $date){ 
-                             foreach($volunteer->histories as $history) {
-                                if($history->date != $eventDate ){
-                                    $history = new History();
-                                    $history->date =  $date;
-                                    $history->count =  1;
-                                    $history->save();
-                                    $history->volunteers()->attach($volunteer->id);
-                                }
-
-                             }
-                        }
-                        
-                    }
-            }
-        }
+       
         
         return view('vol.masaol',[
             'volunteers' => $volunteers,
