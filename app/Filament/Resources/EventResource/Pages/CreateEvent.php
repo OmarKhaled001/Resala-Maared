198300081979f5ee362b2cf3c06190ca3600439f
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-21 14:11:27 +0300

    wadyy

--- app/Filament/Resources/EventResource/Pages/CreateEvent.php
+++ app/Filament/Resources/EventResource/Pages/CreateEvent.php
@@ -2,11 +2,52 @@
 
 namespace App\Filament\Resources\EventResource\Pages;
 
-use App\Filament\Resources\EventResource;
+use Carbon\Carbon;
 use Filament\Actions;
+use App\Models\Volunteer;
+use App\Models\Contribution;
+use Illuminate\Notifications\Action;
+use App\Filament\Resources\EventResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEvent extends CreateRecord
 {
     protected static string $resource = EventResource::class;
+        // Add custom actions
+
+    
+        // Handle post-creation logic
+        protected function handleAfterCreate(Volunteer $record)
+        {
+            $volunteers = Volunteer::all();
+            
+            foreach ($volunteers as $volunteer) {
+                if ($volunteer->events->isNotEmpty()) {
+                    foreach ($volunteer->events as $event) {
+                        $day = Carbon::create($event->date)->format('d');
+                        $month = Carbon::create($event->date)->format('m');
+                        $year = Carbon::create($event->date)->format('Y');
+    
+                        $contribution = Contribution::where('volunteer_id', $volunteer->id)
+                            ->where('year', $year)
+                            ->where('month', $month)
+                            ->first();
+    
+                        if ($contribution) {
+                            $contribution->$day = 1; // Update the specific day
+                            $contribution->save();
+                        } else {
+                            $contribution = new Contribution;
+                            $contribution->volunteer_id = $volunteer->id;
+                            $contribution->year = $year;
+                            $contribution->month = $month;
+                            $contribution->$day = 1; // Set the specific day
+                            $contribution->save();
+                        }
+                    }
+                }
+            }
+        }
+    
+    
 }
