7429ffb093f8d1bc3f0240927a6b0bcc631bb2da
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-23 00:34:24 +0300

    wadyy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
                                 <div class="col">
                                     <div class="card border-bottom border-3 card-animate {{$mtotalContributionsCount < $masaolVolunteers->count() ? 'border-danger' : 'border-success'}}">
                                         <div class="card-body">
-                                            <span class="badge bg-success-subtle text-success float-end">{{ $mtotalContributionsCount}} / {{$masaolVolunteers->count()}}</span>
+                                            <span class="badge{{$mtotalContributionsCount < $masaolVolunteers->count() ? 'bg-danger-subtle text-danger' : 'bg-success-subtle text-success'}} float-end">{{ $mtotalContributionsCount}} / {{$masaolVolunteers->count()}}</span>
                                             <h4 class="mb-4"><span class="counter-value" data-target="{{round($mtotalSum/$masaolVolunteers->count(), 2) }}">0</span> /8</h4>
                                             <p class="text-muted fw-medium text-uppercase mb-0">متوسط مشاركات مسؤول</p>
                                         </div>
