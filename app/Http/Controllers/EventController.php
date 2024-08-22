9eb55df7275f84e7a7e98bd170d3293066f4acef
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-22 16:02:33 +0300

    wadyy

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -131,9 +131,6 @@ class EventController extends Controller
         $currentMonth = now()->month;
 
             
-        $m = Carbon::create($currentMonth)->format('m');
-        $y= Carbon::create($currentYear)->format('Y');
-
         $volunteers = Volunteer::with(['contributions' => function ($query) use ($currentYear, $currentMonth) {
             $query->where('year', $currentYear)->where('month', $currentMonth);
         }])->get();
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Volunteer;
+use App\Models\Contribution;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,8 +26,63 @@ class HomeController extends Controller
      */
     public function index()
     {
+        
         $currentYear = now()->year;
         $currentMonth = now()->month;
+    
+        $volunteers = Volunteer::with(['contributions' => function ($query) use ($currentYear, $currentMonth) {
+            $query->where('year', $currentYear)->where('month', $currentMonth);
+        }])->get();
+       
+        foreach( $volunteers as $volunteer){
+            
+            if($volunteer->events != null){
+                // get all contribution
+                foreach ($volunteer->events as $event) {
+                    $day = Carbon::create($event->date)->format('d');
+                    $month = Carbon::create($event->date)->format('m');
+                    $year= Carbon::create($event->date)->format('Y');
+                    $contribution = Contribution::where('volunteer_id',$volunteer->id)
+                    ->where('year', $year)
+                    ->where('month', $month)
+                    ->get()
+                    ->first();
+                    if($contribution != null){
+                        $contribution->year = $year;
+                        $contribution->month = $month;
+                        $contribution->$day = 1;
+                        $contribution->save();
+                    }else{
+                        $contribution = new Contribution;
+                        $contribution->volunteer_id =$volunteer->id;
+                        $contribution->year = $year;
+                        $contribution->month = $month;
+                        $contribution->$day = 1;
+                        $contribution->save();
+                    }
+                }
+            }
+            if (count($volunteer->contributions)>0){
+                $total = 0;
+                $contribution = $volunteer->contributions->first();
+                for ($i = 1; $i <= 31; $i++){
+    
+                    $day = str_pad($i, 2, '0', STR_PAD_LEFT);
+                    if ($contribution->$day != null){
+                       $total += 1; 
+                    }
+                }
+                $contribution->total = $total; 
+
+                $contribution->save();
+            }
+
+        }
+
+
+
+
+
         $masaolVolunteers = Volunteer::where('status','مسئول')->with(['contributions' => function ($query) use ($currentYear, $currentMonth) {
             $query->where('year', $currentYear)->where('month', $currentMonth);
         }])->get();
