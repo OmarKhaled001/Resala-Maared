5455966ade97f232bb9a13513216d5075af344c1
Omar Khaled <OmarKhaled102002@gmail.com>
2024-08-29 10:14:57 +0300

    wadyy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-            ->locales(['ar','en','fr'])
+            ->locales(['ar'])
             ->visible(outsidePanels: true)
             ->displayLocale('ar'); // also accepts a closure
         });
