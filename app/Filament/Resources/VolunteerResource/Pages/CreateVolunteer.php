106945c0ce09552d970a034af42978b18aa51a5f
Omar Khaled <OmarKhaled102002@gmail.com>
2024-09-05 12:59:23 +0300

    wadyy

--- app/Filament/Resources/VolunteerResource/Pages/CreateVolunteer.php
+++ app/Filament/Resources/VolunteerResource/Pages/CreateVolunteer.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateVolunteer extends CreateRecord
 {
     protected static string $resource = VolunteerResource::class;
+
+    public function getTitle(): string 
+    {
+        return 'إضافة متطوع';
+    }
 }
