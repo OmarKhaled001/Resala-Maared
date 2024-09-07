8aa16a142e44a869be7618e4e9e0357d855cf395
Omar Khaled <OmarKhaled102002@gmail.com>
2024-09-07 11:39:20 +0300

    wadyy

---
+++ app/Filament/Resources/ContributionResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\Contribution;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ContributionResource\Pages;
+use App\Filament\Resources\ContributionResource\RelationManagers;
+
+class ContributionResource extends Resource
+{
+    protected static ?string $model = Contribution::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                
+                TextColumn::make('volunteer.name')
+                ->label('الاسم')
+                ->searchable()
+                ->copyable()
+                ->sortable(),
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContributions::route('/'),
+            'create' => Pages\CreateContribution::route('/create'),
+            'edit' => Pages\EditContribution::route('/{record}/edit'),
+        ];
+    }
+}
